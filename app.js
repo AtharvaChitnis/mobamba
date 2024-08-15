adca20ce21ca78a4efbaf6d46438cb938e7108e4
AtharvaChitnis <atharva.chitnis96@gmail.com>
2024-08-15 19:31:51 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+'use strict';
+
+let x =
+  'This is a message to the developer who is developing this, that whatever is on this file can be edited and made better even this message is looking for something better to be in it. I feel like a noob programmer when I think of myself as a programmer, and I think there is much more potential to me, and this is my insecurity which I am trying to share with my code, my right hand does much less typing than my left hand. Apologies';
+
+console.log(x);
